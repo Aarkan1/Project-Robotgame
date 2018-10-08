fdcf3a7d2c978db8cac1daf424446276d8d27703
Aarkan1 <37023580+Aarkan1@users.noreply.github.com>
2018-10-08 10:15:13 +0200

    lagt till kommentarer

--- Project Robotgame/src/com/game/CheetahRobot.java
+++ Project Robotgame/src/com/game/CheetahRobot.java
@@ -1,22 +1,59 @@
 package com.game;
 
+import java.util.ArrayList;
+
+// subclass from robot
 public class CheetahRobot extends Robot {
 
+    // cheetahs has a hungerstate
+    Hunger hungerState;
+
+    // default constructor
+    // places the robot on the lower right tile
+    // speed is set to fast, hungerstate is by default hungry
     public CheetahRobot() {
-        super(29, 29);
-        Hunger hungerState = Hunger.HUNGRY;
-        setSpeed(2);
+        super(Gameboard.GRID_SIZE - 1, Gameboard.GRID_SIZE - 1);
+        setHungerState(Hunger.HUNGRY);
+        setSpeed(Speed.FAST);
     }
 
+    // constructor for custom placement of the robot
+    // speed is set to fast, hungerstate is by default hungry
     public CheetahRobot(int coordX, int coordY) {
         super(coordX, coordY);
-        Hunger hungerState = Hunger.HUNGRY;
-        setSpeed(2);
+        setHungerState(Hunger.HUNGRY);
+        setSpeed(Speed.FAST);
     }
 
+    public Hunger getHungerState() {
+        return hungerState;
+    }
 
+    public void setHungerState(Hunger hungerState) {
+        this.hungerState = hungerState;
+    }
+
+    // move the robot when called
     @Override
     public void doRun() {
         super.doRun();
+    }
+
+    // function for detecting if the next step has an object or not
+    // returns true if next step is clear
+    @Override
+    public boolean detectCollision(ArrayList<Robot> zebras, ArrayList<Robot> cheetahs) {
+
+        boolean clear = true;
+
+        for (int j = 0; j < cheetahs.size(); j++) {
+            if (cheetahs.get(j) != this) {
+                if ((cheetahs.get(j).getCoordX() == this.getCoordX()) && (cheetahs.get(j).getCoordY() == this.getCoordY())) {
+                    clear = false;
+                    j = cheetahs.size();
+                }
+            }
+        }
+        return clear;
     }
 }
