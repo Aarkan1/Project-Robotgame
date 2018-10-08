dc44cf54340162018078cc03f3ad6feeb630f391
Aarkan1 <37023580+Aarkan1@users.noreply.github.com>
2018-10-08 10:30:01 +0200

    lagt till kommentarer

--- Project Robotgame/src/com/game/IfMovement.java
+++ Project Robotgame/src/com/game/IfMovement.java
@@ -1,8 +1,12 @@
 package com.game;
 
+import java.util.ArrayList;
+
+// interface for movement
 public interface IfMovement {
 
+    // movement methods
     public void doRun();
-
+    public boolean detectCollision(ArrayList<Robot> zebras, ArrayList<Robot> cheetahs);
 
 }
